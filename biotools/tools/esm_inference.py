3c7f5d40009ca642b29cf6259897d27cde5f26fc
guyyanai <26486298+guyyanai@users.noreply.github.com>
2025-07-03 15:24:35 +0300

    updates

--- biotools/tools/esm_inference.py
+++ biotools/tools/esm_inference.py
@@ -6,7 +6,16 @@ from typing import List
 from transformers import EsmTokenizer, EsmModel
 from tqdm import tqdm
 
-def esm3_infer_esm_protein(esm_proteins: List[ESMProtein], esm3: ESM3, projection_head: torch.nn.Sequential, should_normalize=False, use_tqdm=True, tqdm_desc='Inferring proteins using ESM3...'):
+
+def esm3_infer_esm_protein(
+    esm_proteins: List[ESMProtein],
+    esm3: ESM3,
+    projection_head: torch.nn.Sequential,
+    should_normalize=False,
+    move_to_cpu=False,
+    use_tqdm=True,
+    tqdm_desc="Inferring proteins using ESM3...",
+):
     embeddings = []
 
     for esm_protein in tqdm(esm_proteins, desc=tqdm_desc) if use_tqdm else esm_proteins:
@@ -19,19 +28,33 @@ def esm3_infer_esm_protein(esm_proteins: List[ESMProtein], esm3: ESM3, projectio
         embedding = result.mean_embedding
         embeddings.append(embedding)
 
-    embeddings = torch.stack(embeddings)
+    embeddings_tensor = torch.stack(embeddings)
 
     if projection_head:
         with torch.no_grad():
-            embeddings = projection_head(embeddings)
+            embeddings_tensor = projection_head(embeddings_tensor)
 
     if should_normalize:
-        embeddings = F.normalize(embeddings, dim=1)
+        embeddings_tensor = F.normalize(embeddings_tensor, dim=1)
 
-    return embeddings
+    if move_to_cpu:
+        cpu_embeddings = embeddings_tensor.cpu()
+        del embeddings_tensor
+        return cpu_embeddings
+
+    return embeddings_tensor
+
 
 def esm2_infer_sequences(
-    sequences: List[str], esm2: EsmModel, tokenizer: EsmTokenizer, projection_head: torch.nn.Sequential, should_normalize=False, device=None, use_tqdm=True, tqdm_desc='Inferring sequences using ESM2...'
+    sequences: List[str],
+    esm2: EsmModel,
+    tokenizer: EsmTokenizer,
+    projection_head: torch.nn.Sequential,
+    should_normalize=False,
+    device=None,
+    use_tqdm=True,
+    move_to_cpu=False,
+    tqdm_desc="Inferring sequences using ESM2...",
 ):
     if device is None:
         device = torch.device("cuda")
@@ -47,33 +70,85 @@ def esm2_infer_sequences(
         embedding = result.last_hidden_state.mean(dim=1)[0]
         embeddings.append(embedding)
 
-    embeddings = torch.stack(embeddings)
+    embeddings_tensor = torch.stack(embeddings)
 
     if projection_head:
         with torch.no_grad():
-            embeddings = projection_head(embeddings)
+            embeddings_tensor = projection_head(embeddings_tensor)
 
     if should_normalize:
-        embeddings = F.normalize(embeddings, dim=1)
+        embeddings_tensor = F.normalize(embeddings_tensor, dim=1)
 
-    return embeddings
+    if move_to_cpu:
+        cpu_embeddings = embeddings_tensor.cpu()
+        del embeddings_tensor
+        return cpu_embeddings
+
+    return embeddings_tensor
 
 
-def esm3_infer_structures(structures: List[torch.Tensor], esm3: ESM3, projection_head: torch.nn.Sequential, should_normalize=False, use_tqdm=True, tqdm_desc='Inferring structures using ESM3...'):
+def esm3_infer_structures(
+    structures: List[torch.Tensor],
+    esm3: ESM3,
+    projection_head: torch.nn.Sequential,
+    should_normalize=False,
+    move_to_cpu=False,
+    use_tqdm=True,
+    tqdm_desc="Inferring structures using ESM3...",
+):
     esm_proteins = [ESMProtein(coordinates=structure) for structure in structures]
-    return esm3_infer_esm_protein(esm_proteins, esm3, projection_head, should_normalize, use_tqdm, tqdm_desc)
+    return esm3_infer_esm_protein(
+        esm_proteins,
+        esm3,
+        projection_head,
+        should_normalize,
+        move_to_cpu,
+        use_tqdm,
+        tqdm_desc,
+    )
 
 
-def esm3_infer_sequences(sequences: List[str], esm3: ESM3, projection_head: torch.nn.Sequential, should_normalize=False, use_tqdm=True, tqdm_desc='Inferring sequences using ESM3...'):
+def esm3_infer_sequences(
+    sequences: List[str],
+    esm3: ESM3,
+    projection_head: torch.nn.Sequential,
+    should_normalize=False,
+    move_to_cpu=False,
+    use_tqdm=True,
+    tqdm_desc="Inferring sequences using ESM3...",
+):
     esm_proteins = [ESMProtein(sequence=sequence) for sequence in sequences]
-    return esm3_infer_esm_protein(esm_proteins, esm3, projection_head, should_normalize, use_tqdm, tqdm_desc)
+    return esm3_infer_esm_protein(
+        esm_proteins,
+        esm3,
+        projection_head,
+        should_normalize,
+        move_to_cpu,
+        use_tqdm,
+        tqdm_desc,
+    )
 
 
 def esm3_infer_both(
-    sequences: List[str], structures: List[torch.Tensor], esm3: ESM3, projection_head: torch.nn.Sequential, should_normalize=False, use_tqdm=True, tqdm_desc='Inferring sequences+structures using ESM3...'
+    sequences: List[str],
+    structures: List[torch.Tensor],
+    esm3: ESM3,
+    projection_head: torch.nn.Sequential,
+    should_normalize=False,
+    move_to_cpu=False,
+    use_tqdm=True,
+    tqdm_desc="Inferring sequences+structures using ESM3...",
 ):
     esm_proteins = [
         ESMProtein(sequence=sequence, coordinates=structure)
         for sequence, structure in zip(sequences, structures)
     ]
-    return esm3_infer_esm_protein(esm_proteins, esm3, projection_head, should_normalize, use_tqdm, tqdm_desc)
+    return esm3_infer_esm_protein(
+        esm_proteins,
+        esm3,
+        projection_head,
+        should_normalize,
+        move_to_cpu,
+        use_tqdm,
+        tqdm_desc,
+    )
